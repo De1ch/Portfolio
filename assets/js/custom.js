f0b74c003d2a3c0505c1ca13a6e29556dbd8ca3d
De1ch <54121126+De1ch@users.noreply.github.com>
2019-08-17 03:50:43 +0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,7 +6,7 @@
 
 	$(window).load(function() {
 		$('#status').fadeOut();
-		$('#preloader').delay(100).fadeOut('slow');
+		$('#preloader').delay(150).fadeOut('slow');
 	});
 
 	$(document).ready(function() {
